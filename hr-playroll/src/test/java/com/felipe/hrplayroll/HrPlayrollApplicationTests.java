17022f3bacf3c7b9155165089b66bf35679e3bba
Felipe Alves S <felipe.a.desenvolvedor62@gmail.com>
2022-03-16 13:53:00 -0300

    Projeto hr-playroll criado

---
+++ hr-playroll/src/test/java/com/felipe/hrplayroll/HrPlayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.hrplayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPlayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
