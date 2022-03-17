e28134004b0b9e204f3656b48cdfc8c999b36f49
Felipe Alves S <felipe.a.desenvolvedor62@gmail.com>
2022-03-17 15:50:12 -0300

    Comunicação com outro projeto via FeignClient

--- hr-playroll/src/main/java/com/felipe/hrplayroll/service/PaymentService.java
+++ hr-playroll/src/main/java/com/felipe/hrplayroll/service/PaymentService.java
@@ -10,22 +10,17 @@ import org.springframework.web.client.RestTemplate;
 
 import com.felipe.hrplayroll.emtites.Payment;
 import com.felipe.hrplayroll.emtites.Worker;
+import com.felipe.hrplayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost; 
-	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days) {
-		
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workerId);
-		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
